fede265c33ffa114478ed172f4347869d4ffc4c6
munibshah <97636532+munibshah@users.noreply.github.com>
2024-06-23 00:45:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World! Welcome", "message": "Welcome to FastAPI!"}
+
+
+class WebhookPayload(BaseModel):
+    EndTime: str
+
+@app.post("/webhook/")
+async def receive_webhook(data: WebhookPayload):
+    # Extracting EndTime from the payload
+    end_time = data.EndTime
+    print(f"Received EndTime: {end_time}")
+    return {"message": "Data received successfully", "EndTime": end_time}
+
